776e53adce94dca7052f911d366f87f6ae306f5c
MatAssuncao <147439826+MatAssuncao@users.noreply.github.com>
2026-02-09 17:52:26 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_LWv8V11YQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'pt2o89hj0uqhiqtio00pcarvf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_rM35KYEhk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'n5ue74tpsehokfln4tdoh5dd7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
